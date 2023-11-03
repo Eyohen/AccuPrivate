0bec37c910c253d029083548279f4e6a3629c1f9
RealRichi3 <molunorichie@gmail.com>
2023-11-03 09:34:59 +0100

    add types for baxi api response

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,7 +1,6 @@
 
 import dotenv from 'dotenv';
 dotenv.config();  // Load environment variables from .env file 
-console.log(process.env)
 export const BAXI_URL: string | undefined = process.env.BAXI_URL
 export const BAXI_TOKEN: string | undefined = process.env.BAXI_TOKEN
 export const BUYPOWER_URL: string | undefined = process.env.BUYPOWER_URL
--- src/utils/Interface.ts
+++ src/utils/Interface.ts
@@ -14,3 +14,54 @@ export interface IValidateMeter {
 }
 
 
+export interface IBaxiGetProviderResponse {
+    "status": "success",
+    "message": "Successful",
+    "code": 200,
+    "data": {
+        "providers": {
+            "service_type": string,
+            "shortname": string,
+            "biller_id": number,
+            "product_id": number,
+            "name": string
+        }[]
+    }
+}
+
+export interface IBaxiPurchaseResponse {
+    status: string;
+    statusCode: string;
+    message: string;
+    data: {
+        transactionStatus: string;
+        transactionReference: number;
+        statusCode: string;
+        transactionMessage: string;
+        tokenCode: string;
+        tokenAmount: null | number;
+        amountOfPower: null | number;
+        rawOutput?: {
+            fees: {
+                amount: number;
+                kind: string | null;
+                description: string | null;
+                taxAmount: number;
+            }[];
+            feeder: string;
+            dssName: string;
+            serviceBand: string;
+            message: string | null;
+            token: string;
+            rate: string | null;
+            exchangeReference: string;
+            tariff: string | null;
+            power: string | null;
+            resetToken: string;
+            status: string;
+            statusCode: string;
+        };
+        provider_message: string;
+        baxiReference: number;
+    }
+}
