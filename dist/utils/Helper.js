b8b6f00e247a1bbffcab3fd1e341fff5ad49be69
RealRichi3 <molunorichie@gmail.com>
2023-11-13 18:10:26 +0100

    rm build

--- dist/utils/Helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomToken = void 0;
-function generateRandomToken() {
-    return `${Math.floor(Math.random() * 10000)}-${Math.floor(Math.random() * 10000)}-${Math.floor(Math.random() * 10000)}-${Math.floor(Math.random() * 10000)}`;
-}
-exports.generateRandomToken = generateRandomToken;
