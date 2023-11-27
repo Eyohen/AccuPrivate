98e4c4a977cc735f681421b4552124b38d479dfa
RealRichi3 <molunorichie@gmail.com>
2023-11-27 01:04:25 +0100

    clean code in routes

--- src/routes/Notification.routes.ts
+++ src/routes/Notification.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { AuthenticatedController } from "../utils/Interface";
 import NotificationController from "../controllers/Public/Notification.controller"
 
-export const router: Router = Router()
+const router: Router = Router()
 
 router
     .get('/', AuthenticatedController(NotificationController.getNotifications))
--- src/routes/TeamMember.routes.ts
+++ src/routes/TeamMember.routes.ts
@@ -1,5 +1,4 @@
 import express, { Router } from "express";
-import PowerUnitController from "../controllers/Public/Powerunit.controller";
 import { basicAuth } from "../middlewares/Auth";
 import { AuthenticatedController } from "../utils/Interface";
 import TeamMemberProfileController from "../controllers/Public/TeamMember.controller";
