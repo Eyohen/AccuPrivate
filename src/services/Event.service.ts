b12de8012eb20672104d857b3431992abb9e028f
RealRichi3 <molunorichie@gmail.com>
2023-12-06 09:05:16 +0100

    add: TransactionEventService

--- src/services/Event.service.ts
+++ src/services/Event.service.ts
@@ -1,8 +1,155 @@
 // Import necessary types and the Event model
-import { ICreateEvent, IEvent } from "../models/Event.model";
+import { TOPICS } from "../kafka/Constants";
+import { ICreateEvent, IEvent, Status } from "../models/Event.model";
 import Event from "../models/Event.model";
 import Transaction from "../models/Transaction.model";
 import logger from "../utils/Logger";
+import { v4 as uuidv4 } from 'uuid';
+
+interface IMeterValidationReceivedEventParams {
+    user: { name: string, email: string, address: string, phoneNumber: string };
+}
+
+interface ICRMUserInitiatedEventParams {
+    user: { id: string, name: string, email: string, address: string, phoneNumber: string };
+}
+
+interface EventMeterInfo {
+    meterNumber: string,
+    disco: string,
+    vendType: 'PREPAID' | 'POSTPAID'
+}
+
+class TransactionEventService {
+    private transaction: Transaction;
+    private meterInfo: EventMeterInfo
+
+    constructor(transaction: Transaction, meterInfo: EventMeterInfo) {
+        this.transaction = transaction;
+        this.meterInfo = meterInfo;
+    }
+
+    public async addMeterValidationRequestedEvent(): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.METER_VALIDATION_REQUESTED,
+            eventText: TOPICS.METER_VALIDATION_REQUESTED,
+            eventData: JSON.stringify({
+                meterNumber: this.meterInfo.meterNumber,
+                disco: this.meterInfo.disco,
+                vendType: this.meterInfo.vendType,
+            }),
+            source: 'API',
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+
+    public async addMeterValidationResponseEvent(userInfo: IMeterValidationReceivedEventParams): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.METER_VALIDATION_RECIEVED,
+            eventText: TOPICS.METER_VALIDATION_RECIEVED,
+            eventData: JSON.stringify({
+                user: {
+                    name: userInfo.user.name,
+                    email: userInfo.user.email,
+                    address: userInfo.user.address,
+                    phoneNumber: userInfo.user.phoneNumber,
+                },
+                meterNumber: this.meterInfo.meterNumber,
+                disco: this.meterInfo.disco,
+                vendType: this.meterInfo.vendType,
+            }),
+            source: this.transaction.superagent.toUpperCase(),
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+
+    public async addMeterValidationSentEvent(meterId: string): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.METER_VALIDATION_SENT,
+            eventText: TOPICS.METER_VALIDATION_SENT,
+            eventData: JSON.stringify({
+                merterId: meterId,
+                meterNumber: this.meterInfo.meterNumber,
+                disco: this.meterInfo.disco,
+                vendType: this.meterInfo.vendType,
+            }),
+            source: 'API',
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+
+    public async addCRMUserInitiatedEvent(info: ICRMUserInitiatedEventParams): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.CRM_USER_INITIATED,
+            eventText: TOPICS.CRM_USER_INITIATED,
+            eventData: JSON.stringify({
+                user: {
+                    id: info.user.id,
+                    name: info.user.name,
+                    email: info.user.email,
+                    address: info.user.address,
+                    phoneNumber: info.user.phoneNumber,
+                },
+            }),
+            source: 'API',
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+
+    public async addCRMUserConfirmedEvent(info: ICRMUserInitiatedEventParams): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.CRM_USER_CONFIRMED,
+            eventText: TOPICS.CRM_USER_CONFIRMED,
+            eventData: JSON.stringify({
+                user: {
+                    id: info.user.id,
+                    name: info.user.name,
+                    email: info.user.email,
+                    address: info.user.address,
+                    phoneNumber: info.user.phoneNumber,
+                },
+            }),
+            source: 'API',
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+
+    public async addDiscoUpEvent(): Promise<Event> {
+        const event: ICreateEvent = {
+            transactionId: this.transaction.id,
+            eventType: TOPICS.DISCO_UP,
+            eventText: TOPICS.DISCO_UP,
+            eventData: JSON.stringify({
+                disco: this.meterInfo.disco,
+            }),
+            source: 'API',
+            eventTimestamp: new Date(),
+            id: uuidv4(),
+            status: Status.COMPLETE,
+        }
+        return EventService.addEvent(event);
+    }
+}
 
 // EventService class for handling event-related operations
 export default class EventService {
@@ -42,4 +189,6 @@ export default class EventService {
             logger.info('Error reading Event');
         }
     }
+
+    static transactionEventService = TransactionEventService;
 }
