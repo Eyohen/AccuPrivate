009670e2e37f64efd2af24fbd2392d4207a755c1
RealRichi3 <molunorichie@gmail.com>
2023-12-13 16:03:45 +0100

    add: failedTokenRequest handler in notification consumer

--- src/kafka/modules/consumers/Notification.ts
+++ src/kafka/modules/consumers/Notification.ts
@@ -82,8 +82,72 @@ class NotificationHandler extends Registry {
         return
     }
 
+    private static async failedTokenRequest(data: PublisherEventAndParameters[TOPICS.TOKEN_REQUEST_FAILED]) {
+        logger.info('Inside notification handler')
+        const transaction = await TransactionService.viewSingleTransaction(data.transactionId)
+        if (!transaction) {
+            logger.error(`Error fetching transaction with id ${data.transactionId}`)
+            return
+        }
+
+        const partnerEntity = await EntityService.viewSingleEntityByEmail(transaction.partner.email)
+        if (!partnerEntity) {
+            logger.error(`Error fetching partner with email ${transaction.partner.email}`)
+            return
+        }
+
+        // Add notification successfull transaction
+        const notification = await NotificationService.addNotification({
+            id: uuidv4(),
+            title: "Failed transaction",
+            heading: "Failed ransaction",
+            message: `
+                Failed transaction for ${data.meter.meterNumber} with amount ${transaction.amount}
+
+                Bank Ref: ${transaction.bankRefId}
+                Bank Comment: ${transaction.bankComment}
+                Transaction Id: ${transaction.id},
+                `,
+            entityId: partnerEntity.id,
+            read: false,
+        });
+
+        // Check if notifiecations have been sent to partner and user
+        const notifyPartnerEvent = await EventService.viewSingleEventByTransactionIdAndType(transaction.id, TOPICS.TOKEN_REQUEST_FAILED)
+        const notifyUserEvent = await EventService.viewSingleEventByTransactionIdAndType(transaction.id, TOPICS.TOKEN_REQUEST_FAILED)
+
+        const transactionEventService = new TransactionEventService(transaction, {
+            meterNumber: data.meter.meterNumber,
+            vendType: transaction.meter.vendType,
+            disco: data.meter.disco,
+        })
+
+        // If you've not notified the partner before, notify them
+        if (!notifyPartnerEvent) {
+            await NotificationUtil.sendNotificationToUser(
+                partnerEntity.id,
+                notification
+            );
+            await transactionEventService.addTokenRequestFailedNotificationToPartnerEvent()
+        }
+
+        // If you've not notified the user before, notify them
+        if (!notifyUserEvent) {
+            await EmailService.sendEmail({
+                to: transaction.user.email,
+                subject: "Token Purchase",
+                html: await new EmailTemplate().failedTransaction({
+                    transaction: transaction,
+                }),
+            })
+            await transactionEventService.addTokenRequestFailedNotificationToPartnerEvent()
+        }
+        return
+    }
+
     static registry = {
-        [TOPICS.TOKEN_RECIEVED_FROM_VENDOR]: this.handleReceivedToken
+        [TOPICS.TOKEN_RECIEVED_FROM_VENDOR]: this.handleReceivedToken,
+        [TOPICS.TOKEN_REQUEST_FAILED]: this.failedTokenRequest
     }
 }
 
