4202a472a3026c7f052de2c2b944035b172d9566
RealRichi3 <molunorichie@gmail.com>
2023-12-21 16:24:20 +0100

    update kafka connection

--- src/kafka/config/index.ts
+++ src/kafka/config/index.ts
@@ -1,12 +1,25 @@
-import kafka from 'kafkajs'
-import { KAFKA_BROKER, KAFKA_CLIENT_ID } from '../../utils/Constants'
+import kafka from "kafkajs";
+import {
+    KAFKA_BROKER,
+    KAFKA_CLIENT_ID,
+    KAFKA_PASSWORD,
+    KAFKA_USERNAME,
+} from "../../utils/Constants";
 
 const kafkaConfig: kafka.KafkaConfig = {
     clientId: KAFKA_CLIENT_ID,
     brokers: [KAFKA_BROKER],
-    logLevel: 0
-}
+    connectionTimeout: 4500,
+    ssl: true,
+    sasl: {
+        mechanism: "plain",
+        username: KAFKA_USERNAME,
+        password: KAFKA_PASSWORD,
+    },
+    logLevel: 0,
+};
 
-const Kafka = new kafka.Kafka(kafkaConfig)
+const Kafka = new kafka.Kafka(kafkaConfig);
 
-export default Kafka
+export default Kafka;
+
