ebcf615e813dcde27705de1a823f483e683d8bb9
RealRichi3 <molunorichie@gmail.com>
2024-01-01 10:49:20 +0100

    clean code

--- src/middlewares/Rbac.ts
+++ src/middlewares/Rbac.ts
@@ -6,7 +6,6 @@ import { ForbiddenError } from "../utils/Errors";
 export default class RBACMiddelware {
     static validateRole = (allowedRoles: RoleEnum[]) => {
         return AuthenticatedController(async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-            console.log(req.user.user.entity)
             const userIsPermitted = allowedRoles.includes(req.user.user.entity.role)
             if (!userIsPermitted) {
                 throw new ForbiddenError('Unauthorized access')
