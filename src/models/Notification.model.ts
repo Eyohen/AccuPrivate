38ef24348abcad8ce233debc4ef27381711e0547
RealRichi3 <molunorichie@gmail.com>
2024-01-13 12:09:15 +0100

    fix: 1hr delay in createdAt for Notification controller

--- src/models/Notification.model.ts
+++ src/models/Notification.model.ts
@@ -1,14 +1,15 @@
 import {
-  Table,
-  Column,
-  Model,
-  DataType,
-  IsUUID,
-  PrimaryKey,
-  BelongsTo,
-  HasOne,
-  ForeignKey,
-  BeforeCreate,
+    Table,
+    Column,
+    Model,
+    DataType,
+    IsUUID,
+    PrimaryKey,
+    BelongsTo,
+    HasOne,
+    ForeignKey,
+    BeforeCreate,
+    BeforeSave,
 } from "sequelize-typescript";
 import Entity from "./Entity/Entity.model";
 import Event from "./Event.model";
@@ -16,75 +17,70 @@ import { NigerianDate } from "../utils/Date";
 
 @Table
 export default class Notification extends Model<INotification | Notification> {
-  @IsUUID(4)
-  @PrimaryKey
-  @Column
-  id: string;
+    @IsUUID(4)
+    @PrimaryKey
+    @Column
+    id: string;
 
-  @Column({ type: DataType.STRING, allowNull: false })
-  title: string;
+    @Column({ type: DataType.STRING, allowNull: false })
+    title: string;
 
-  @Column({ type: DataType.TEXT, allowNull: false })
-  message: string;
+    @Column({ type: DataType.TEXT, allowNull: false })
+    message: string;
 
-  @Column({ type: DataType.STRING, allowNull: false })
-  heading: string;
+    @Column({ type: DataType.STRING, allowNull: false })
+    heading: string;
 
-  @Column({ type: DataType.BOOLEAN, allowNull: false, defaultValue: false })
-  read: boolean;
+    @Column({ type: DataType.BOOLEAN, allowNull: false, defaultValue: false })
+    read: boolean;
 
-  @ForeignKey(() => Event)
-  @IsUUID(4)
-  @Column({ type: DataType.STRING, allowNull: true })
-  eventId: string;
+    @ForeignKey(() => Event)
+    @IsUUID(4)
+    @Column({ type: DataType.STRING, allowNull: true })
+    eventId: string;
 
-  @BelongsTo(() => Event)
-  event: Event;
+    @BelongsTo(() => Event)
+    event: Event;
 
-  @ForeignKey(() => Entity)
-  @IsUUID(4)
-  @Column({ type: DataType.STRING, allowNull: false })
-  entityId: string;
+    @ForeignKey(() => Entity)
+    @IsUUID(4)
+    @Column({ type: DataType.STRING, allowNull: false })
+    entityId: string;
 
-  @BelongsTo(() => Entity)
-  entity: Entity;
+    @BelongsTo(() => Entity)
+    entity: Entity;
 
-  @Column({
-    type: DataType.DATE,
-    allowNull: false,
-    defaultValue: new Date(),
-  })
-  createdAt: Date;
-
-  @Column({
-    type: DataType.DATE,
-    allowNull: false,
-    defaultValue: new NigerianDate().getCurrentNigerianDate(),
-  })
-  updatedAt: Date;
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+        defaultValue: new NigerianDate().getCurrentNigerianDate().toString(),
+    })
+    timeStamp: string;
 }
 
 enum NotificationType {
-  LOGIN = "LOGIN",
-  LOGOUT = "LOGOUT",
-  FAILED_TRANSACTION = "FAILED_TRANSACTION",
-  NEW_MEMBER_ACCOUNT = "NEW_MEMBER_ACCOUNT",
+    LOGIN = "LOGIN",
+    LOGOUT = "LOGOUT",
+    FAILED_TRANSACTION = "FAILED_TRANSACTION",
+    NEW_MEMBER_ACCOUNT = "NEW_MEMBER_ACCOUNT",
 }
 
 export interface INotification {
-  id: string;
-  title: string;
-  message: string;
-  heading: string;
-  read: boolean;
-  eventId?: string;
-  entityId: string;
+    id: string;
+    title: string;
+    message: string;
+    heading: string;
+    read: boolean;
+    eventId?: string;
+    entityId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
-export interface ICreateNotification extends INotification {}
+export interface ICreateNotification extends INotification { }
 
 export interface IUpdateNotification {
-  title?: string;
-  message?: string;
-  heading?: string;
+    title?: string;
+    message?: string;
+    heading?: string;
 }
