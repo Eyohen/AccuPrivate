6a889ca8fe37b33a75ed4684f1e460cebc746680
RealRichi3 <molunorichie@gmail.com>
2024-02-15 16:50:18 +0100

    add: sms template

--- src/utils/Sms.ts
+++ src/utils/Sms.ts
@@ -1,6 +1,7 @@
 import AfricasTalking from 'africastalking'
 import { AFRICASTALKING_API_KEY, AFRICASTALKING_USERNAME, NODE_ENV } from './Constants';
 import axios from 'axios'
+import Transaction from '../models/Transaction.model';
 
 const client = axios.create({
     baseURL: NODE_ENV == "development" ? "https://api.sandbox.africastalking.com/version1/messaging" : "https://api.sandbox.africastalking.com/version1/messaging",
@@ -24,5 +25,39 @@ export class SmsService {
         } catch (ex) {
             throw ex;
         }
+    }
+
+    static prepaidElectricityTemplate = async (transaction: Transaction) => {
+        const powerUnit = await transaction.$get('powerUnit')
+
+        return `
+            Payment successful for ${transaction.transactionType}
+
+            Transaction amount: ${transaction.amount}
+
+            Token: ${powerUnit?.token}
+
+            Date: ${transaction.transactionTimestamp}
+        `
+    }
+
+    static postpaidElectricityTemplate = async (transaction: Transaction) => {
+        return `
+            Payment successful for ${transaction.transactionType}
+
+            Transaction amount: ${transaction.amount}
+
+            Date: ${transaction.transactionTimestamp}
+        `
+    }
+
+    static airtimeTemplate = async (transaction: Transaction) => {
+        return `
+            Payment successful for ${transaction.transactionType}
+
+            Transaction amount: ${transaction.amount}
+
+            Date: ${transaction.transactionTimestamp}
+        `
     }
 }
