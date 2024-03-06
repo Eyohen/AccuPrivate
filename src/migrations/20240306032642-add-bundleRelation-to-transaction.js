621988d3759014077926fb561be99bed7bfba4d1
RealRichi3 <molunorichie@gmail.com>
2024-03-06 05:07:27 +0100

    update

---
+++ src/migrations/20240306032642-add-bundleRelation-to-transaction.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+
+        // Add bundleRelation to transaction as bundleId, it should be optional and it should relate with bunlde table
+        await queryInterface.addColumn('transactions', 'bundleId', {
+            type: Sequelize.STRING,
+            references: {
+                model: 'bundles',
+                key: 'id'
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+        await queryInterface.removeColumn('transactions', 'bundleId');
+    }
+};
