39a92efa18dfee29122d7f81a11406c5c89903c2
RealRichi3 <molunorichie@gmail.com>
2024-03-25 16:37:37 +0100

    update

--- src/kafka/modules/consumers/Token.ts
+++ src/kafka/modules/consumers/Token.ts
@@ -89,15 +89,15 @@ const TransactionErrorCodeAndCause = {
 export async function getCurrentWaitTimeForRequeryEvent(retryCount: number) {
     // Time in seconds
     // const defaultValues = [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960, 81920, 163840, 327680, 655360, 1310720, 2621440, 5242880]
-    // const defaultValues = [120, 120, 120] // Default to 2mins because of buypowerng minimum wait time for requery
-    // const timesToRetry = defaultValues
-    // timesToRetry.unshift(1)
+    const defaultValues = [0, 120] // Default to 2mins because of buypowerng minimum wait time for requery
+    const timesToRetry = defaultValues
+    timesToRetry.unshift(1)
 
-    // if (retryCount >= timesToRetry.length) {
-    //     return timesToRetry[timesToRetry.length - 1]
-    // }
+    if (retryCount >= timesToRetry.length) {
+        return timesToRetry[timesToRetry.length - 1]
+    }
 
-    return 120
+    return timesToRetry[retryCount]
 }
 
 
